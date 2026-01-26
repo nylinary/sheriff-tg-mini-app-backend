ff0fabc669c9850139fadd45ae4111fb25f8bb1d
Nikita Sysoev <nylinary.work@yandex.com>
2026-01-27 00:48:54 +0300

    dev Update requirements and fix alembic command in start script

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ PORT="${PORT:-8000}"
 LOG_LEVEL="${LOG_LEVEL:-info}"
 
 # Run DB migrations (safe to run on every start)
-alembic upgrade head
+python -m alembic upgrade head
 
 exec uvicorn main:app --host 0.0.0.0 --port "${PORT}" --log-level "${LOG_LEVEL}"
